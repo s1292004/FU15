d0e9dbdc95d58b841ab3f082f1b18047438a1425
yoshiki sakai <s1292004@u-aiz.ac.jp>
2023-06-09 12:04:39 +0900

    Greetings added

--- ex1/dice.cpp
+++ ex1/dice.cpp
@@ -6,23 +6,34 @@ using namespace std;
 class DiceGame {
     private:
         int r1,r2;
+        int total;
+        string name;
 
     public:
         DiceGame() {
             srand(time(0));
             r1 = rand()%6 + 1;
             r2 = rand()%6 + 1;
+            total = r1 + r2;
         }
         int getValue(int id) { return (id > 2 || id <= 0)? 0: (id == 1)? r1:r2; }
         int getTotal() { return r1+r2; }
+
+        void play() {
+            cout << "What is your name?\n" << "> ";
+            char ch;
+            while(cin.get(ch) && ch != '\n') name += ch;
+            cout << "Hello, " << name << "!" << endl;
+
+            cout << "Rolling dice..." << endl;
+            cout << r1 << endl;
+            cout << r2 << endl;
+            cout << "total: " << total << endl;
+        }
 };
 
 int main(void) {
     DiceGame game;
-
-    cout << "Rolling dice..." << endl;
-    cout << game.getValue(1) << endl;
-    cout << game.getValue(2) << endl;
-    cout << "total: " << game.getTotal() << endl;
+    game.play();
     return 0;
 }
